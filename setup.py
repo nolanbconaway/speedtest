fb34476ddd6a3d893af04497cce14d0bc10733e7
Nolan Conaway <nolan@mbp.local>
2019-05-27 17:53:53 -0400

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name="speedtestimage",
     packages=["speedtestimage"],
-    version="0.1",
+    version="0.1.1",
     install_requires=["speedtest-cli", "pillow"],
     scripts=["speedtestimage/speedtestimage"],
     package_data={"speedtestimage": ["Courier.ttf"]},
